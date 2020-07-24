ef27f35d0f59afcbd868868f11ae82604514481e
Zaitsev <yurist.feo@gmail.com>
2020-07-24 11:10:14 +0300

    start commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+import "@fortawesome/fontawesome-free/css/all.min.css"
+import "./resources/scss/all.scss"
+
+import "jquery";
+import "bootstrap/dist/js/bootstrap.js";
+import "bootstrap/dist/css/bootstrap.css";
+
+
+ReactDOM.render(<App/>, document.getElementById("root"));
+
