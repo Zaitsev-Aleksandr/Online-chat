ad189fc8213180d1d6c340108a96d99b7b61031a
Zaitsev <yurist.feo@gmail.com>
2020-07-24 12:48:12 +0300

    minor config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
 import "@fortawesome/fontawesome-free/css/all.min.css"
 import "./resources/scss/all.scss"
 
@@ -11,4 +10,3 @@ import "bootstrap/dist/css/bootstrap.css";
 
 
 ReactDOM.render(<App/>, document.getElementById("root"));
-
